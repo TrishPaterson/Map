e326574e632f414b22174f5b914d4dee3aa6757a
Daniel Paterson <Daniel Paterson@192.168.1.8>
2016-08-15 20:41:04 +1200

    I've added the expanding zone, which increases in size, but doesn't stop.

--- src/map/Map.java
+++ src/map/Map.java
@@ -6,11 +6,11 @@
 package map;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+//import javafx.event.ActionEvent;
+//import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+//import javafx.scene.control.Button;
+//import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.scene.web.WebEngine; 
