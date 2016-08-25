0e5f8ae160f4c0e21cf4ac43e7903c62a478ad48
Jay <>
2016-08-25 23:49:25 +1200

    Made changes on Unit Class and added a UnitInfo XML I will use this to populate each info for units.

--- src/map/Unit.java
+++ src/map/Unit.java
@@ -1,78 +1,112 @@
 package map;
+/* 
+Code might change in the future when GUI backened and Unit.JavaScript is done.
+*/
+import org.xml.sax.*;
+import org.w3c.dom.*;
+import javax.xml.parsers.*;
 
 public class Unit {
-	// this enum values are temporary
-	protected enum Type{car1,car2}; 
-	protected enum Status{available,Onroute,Onscene,unavailable}	
-	
-	private String callSign;
-	private String currLocation;
-	private String currEvent;
-	private String time;
-	private Type type;
-	private Status status;
+    protected enum Type { // Not Final Object Names 
+        car1("P10"), car2("P20");
+        
+        private String car;
+        Type(String car){
+            this.car = car;
+        }
+        public String getType(){
+            return car;
+        }
+    };
 
-	Unit(){ //temporary values
-		this.callSign = "BatSign";
-	    this.type = type.car1;
-		this.time = "00:00:00";
-		this.currLocation = "Gotham City";
-		this.status = status.available;		
-	}
-	
-	Unit(String callSign, String time, String currLocation, Status status){
-		this.callSign = callSign;
-		this.time = time;
-		this.currLocation = currLocation;
-		this.status = status;
-	}
+    protected enum Status { // Not Final Object Names 
+        avail("available"), onRot("Onroute"), 
+        onSen("Onscene"), unAvail("unavailable");
+        
+        private String status;   
+        Status(String status){
+            this.status = status;
+        }
+        
+        public String getStatus() {
+            return status;
+        }
+    }
+
+    private String currLocation;
+    private String uniqId;
+    private String callSign;
+    private String defLocation;
+    private String currEvent;
+    private String time;
+    private Type type;
+    private Status status;
+
+    Unit(String uniqId, String callSign, String defLocation, String currEvent, String time, String type, String stat) {
+        this.uniqId = uniqId;
+        this.callSign = callSign;
+        this.defLocation = defLocation;
+        this.currEvent = currEvent;
+        this.time = time; //not final TBD 'Type'
+        this.type = setType(type);
+        this.status = setStatus(stat);
+    }
+    
 //setters
-	public void setCallSign(String callSign){
-		this.callSign = callSign;
-	}
-	
-	public void setCurrLocation(String currLocation){
-		this.currLocation = currLocation;
-	}
-	
-	public void setCurrEvent(String currEvent){
-		this.currEvent = currEvent;
-	}
-	
-	public void setTime(String time){
-		this.time = time;
-	}
-	
-	public void setStatus(Status status){
-		this.status = status;
-	}
-	
-	public void setType(Type type){
-		this.type = type;
-	}
-	
-//getters	
-	public String getCallSign(){
-		return callSign;
-	}
-	
-	public String getCurrLocation(){
-		return currLocation;
-	}
-	
-	public String getCurrEvent(){
-		return currEvent;
-	}
-	
-	public String getTime(){
-		return time;
-	}
-	
-	public Status getStatus(){
-		return status;
-	}
-	
-	public Type getType(){
-		return type;
-	}
+    public String changeLocation(String unitID){ //might become static
+        return("changeLocation('unitID')"); //not final return type
+    }
+    
+    public String setFirstLocation(String unitID) {//might become static  
+        return("setCurrLocation('unitID')"); //not final return type
+    }
+    
+    public String setCurrEvent(String unitID) { //might become static
+        return("setEvent('unitID')"); //not final return type
+    }
+
+    public void setTime(String time) {
+        this.time = time; //not final TBD Type
+    }
+    
+    public void setCallSign(String callSign) {
+        this.callSign = callSign;
+    }
+         
+    public Status setStatus(String status){ //may change
+        switch(status){
+            case "available": return this.status.avail; 
+            case "unavailable": return this.status.unAvail; 
+            case "unroute": return this.status.onRot; 
+            case "onscene": return this.status.onSen; 
+            default: break;
+        }
+        return null;
+    }   
+    
+    public Type setType(String type){ //may change  
+        switch(type){
+            case "P10": return this.type.car1;
+            case "P20": return this.type.car2;
+            default: break;
+        }
+        return null;
+    }   
+    
+//getters   
+    public String getCallSign() {
+        return callSign;
+    }
+
+    public String getCurrLocation() {
+        return currLocation;
+    }
+
+    public String getCurrEvent() {
+        return currEvent;
+    }
+
+    public String getTime() {
+        return time;
+    }
 }
