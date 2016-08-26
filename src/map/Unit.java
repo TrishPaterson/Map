732d51527c28b182e0493fe64a4f94fe947d9833
Jay <Baipitt@192.168.1.3>
2016-08-26 18:40:32 +1200

    Added some getters on unit class. made minor changes on UnitInfo file. Removed defLocation replaced it to currLocation

--- src/map/Unit.java
+++ src/map/Unit.java
@@ -2,9 +2,6 @@ package map;
 /* 
 Code might change in the future when GUI backened and Unit.JavaScript is done.
 */
-import org.xml.sax.*;
-import org.w3c.dom.*;
-import javax.xml.parsers.*;
 
 public class Unit {
     protected enum Type { // Not Final Object Names 
@@ -20,8 +17,8 @@ public class Unit {
     };
 
     protected enum Status { // Not Final Object Names 
-        avail("available"), onRot("Onroute"), 
-        onSen("Onscene"), unAvail("unavailable");
+        avail("Available"), onRot("Onroute"), 
+        onSen("Onscene"), unAvail("Unavailable");
         
         private String status;   
         Status(String status){
@@ -33,19 +30,19 @@ public class Unit {
         }
     }
 
-    private String currLocation;
-    private String uniqId;
+    private final String uniqId;
     private String callSign;
-    private String defLocation;
+    private String currLocation;
+    //private String defLocation;
     private String currEvent;
     private String time;
-    private Type type;
+    private final Type type;
     private Status status;
 
     Unit(String uniqId, String callSign, String defLocation, String currEvent, String time, String type, String stat) {
         this.uniqId = uniqId;
         this.callSign = callSign;
-        this.defLocation = defLocation;
+        this.currLocation = defLocation;
         this.currEvent = currEvent;
         this.time = time; //not final TBD 'Type'
         this.type = setType(type);
@@ -68,17 +65,13 @@ public class Unit {
     public void setTime(String time) {
         this.time = time; //not final TBD Type
     }
-    
-    public void setCallSign(String callSign) {
-        this.callSign = callSign;
-    }
          
     public Status setStatus(String status){ //may change
         switch(status){
-            case "available": return this.status.avail; 
-            case "unavailable": return this.status.unAvail; 
-            case "unroute": return this.status.onRot; 
-            case "onscene": return this.status.onSen; 
+            case "avail": return this.status.avail; 
+            case "unAvail": return this.status.unAvail; 
+            case "onRot": return this.status.onRot; 
+            case "onSen": return this.status.onSen; 
             default: break;
         }
         return null;
@@ -86,14 +79,18 @@ public class Unit {
     
     public Type setType(String type){ //may change  
         switch(type){
-            case "P10": return this.type.car1;
-            case "P20": return this.type.car2;
+            case "car1": return this.type.car1;
+            case "car2": return this.type.car2;
             default: break;
         }
         return null;
     }   
     
-//getters   
+//getters    
+    public String getId(){
+        return uniqId;
+    }
+    
     public String getCallSign() {
         return callSign;
     }
@@ -108,5 +105,13 @@ public class Unit {
 
     public String getTime() {
         return time;
+    }
+
+    public String getType(){
+        return type.getType();
+    }
+    
+    public String getStatus(){
+        return status.getStatus();
     }
 }
