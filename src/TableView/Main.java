623c3dc89d0e256ccf01d2a28e1221dc9556f9c9
Baipitt <Baipitt@192.168.1.5>
2016-09-11 00:04:01 +1200

    This is demo that shows the connection between the unit class and unit table GUI.

---
+++ src/TableView/Main.java
@@ -0,0 +1,161 @@
+package TableView;
+
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+import javafx.stage.Stage;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+public class Main extends Application {
+    private Stage Mapwindow;
+    private Stage Listwindow;
+    private TableView<Units> table;
+    private static String uniqID;
+    private static String callSign;
+    private static String defLocation;
+    private static String currEvent;
+    private static String time;
+    private static String type;
+    private static String status;
+
+    private static Units[] x = null;
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+    @Override
+    public void start(Stage primaryStage)throws ParserConfigurationException, SAXException, IOException{       
+        //Load Map
+        WebView browser = new WebView();
+        WebEngine webEngine = browser.getEngine();
+        webEngine.load(getClass().getResource("mapHtmlFile.html").toString());
+
+        Mapwindow = new Stage();
+        Mapwindow.setTitle("Web Map");
+        Mapwindow.setScene(new Scene(browser,1000,700, Color.web("#666970")));
+        Mapwindow.show();
+        
+        //Load UnitTable
+        Listwindow = primaryStage;
+        Listwindow.setTitle("Unit Table");
+
+        createTable();
+        createContextMenu();
+        
+        VBox vBox = new VBox();
+        vBox.getChildren().addAll(table);   
+        Scene scene = new Scene(vBox);
+        Listwindow.setScene(scene);
+        Listwindow.show();
+    }
+    
+    public void createContextMenu(){
+        ObservableList<Units> unitSelected, allUnits;   
+        allUnits = table.getItems();
+        unitSelected = table.getSelectionModel().getSelectedItems(); 
+     
+        //ContextMenu
+        ContextMenu contextMenu = new ContextMenu();
+        
+        //menu items
+        MenuItem item1 = new MenuItem("ChangeLocation");
+        MenuItem item2 = new MenuItem("Dispatch");
+        
+        //Add item to context menu
+        contextMenu.getItems().addAll(item1, item2);
+        
+        //set context menu to table
+        table.setContextMenu(contextMenu);
+  
+        //changeLocationFunction
+        item1.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent e) {
+                
+                // reminder dont forget to change x[0] 
+                //this line of code is not doing anything it just prints out the javascript command
+                // in the future this will be change into 
+                //executeScript(x[i].changeLocation(unitSelected.get(0).getId()));
+                System.out.println(x[0].changeLocation(unitSelected.get(0).getId()));
+            }
+        });
+        
+        //dispatchLocation function
+        item2.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent e) {
+                System.out.println("IM Batman");
+            }
+        });       
+    }
+    
+    public void createTable()throws ParserConfigurationException, SAXException, IOException{
+        //ID column
+        TableColumn<Units, String> uniqIdCol = new TableColumn<>("ID");
+        uniqIdCol.setMinWidth(100);
+        uniqIdCol.setCellValueFactory(new PropertyValueFactory<>("id"));
+        //Location column
+        TableColumn<Units, String> currLocationCol = new TableColumn<>("Location");
+        currLocationCol.setMinWidth(300);
+        currLocationCol.setCellValueFactory(new PropertyValueFactory<>("currLocation"));
+        //Time column
+        TableColumn<Units, String> timeCol = new TableColumn<>("Time");
+        timeCol.setMinWidth(100);
+        timeCol.setCellValueFactory(new PropertyValueFactory<>("time"));
+        //Type column
+        TableColumn<Units, String> typeCol = new TableColumn<>("Type");
+        typeCol.setMinWidth(100);
+        typeCol.setCellValueFactory(new PropertyValueFactory<>("type"));
+    
+        //create Table
+        table = new TableView<>();
+        table.setItems(getUnits());
+        table.getColumns().addAll(uniqIdCol,currLocationCol,timeCol,typeCol);        
+    }
+    
+    public ObservableList<Units> getUnits()throws ParserConfigurationException, SAXException, IOException{
+        
+        ObservableList<Units> unit = FXCollections.observableArrayList();    
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document doc = builder.parse("Test.xml");
+        NodeList nList = doc.getElementsByTagName("unit");
+        
+        x = new Units[nList.getLength()];
+        for (int i = 0; i < nList.getLength(); i++) {
+            Node node = nList.item(i);
+            if (node.getNodeType() == Node.ELEMENT_NODE) {
+                Element eElement = (Element) node;
+                uniqID = eElement.getAttribute("id");
+                callSign = eElement.getElementsByTagName("callSign").item(0).getTextContent();
+                defLocation = eElement.getElementsByTagName("defLocation").item(0).getTextContent();
+                currEvent = eElement.getElementsByTagName("currEvent").item(0).getTextContent();
+                time = eElement.getElementsByTagName("time").item(0).getTextContent();
+                type = eElement.getElementsByTagName("type").item(0).getTextContent();
+                status = eElement.getElementsByTagName("status").item(0).getTextContent();
+                x[i] = new Units(uniqID, callSign, defLocation, currEvent, time, type, status);
+                unit.add(x[i]);
+            }
+        }
+        return unit;
+    }
+}
---
+++ src/TableView/Units.java
@@ -0,0 +1,135 @@
+package TableView;
+/* 
+Code might change in the future when GUI backened and Unit.JavaScript is done.
+*/
+
+public class Units {
+    protected enum Type { // Not Final Object Names 
+        car1("P10"), car2("P20");
+        
+        private String car;
+        Type(String car){
+            this.car = car;
+        }
+        public String getType(){
+            return car;
+        }
+    };
+
+    protected enum Status { // Not Final Object Names 
+        avail("Available"), onRot("Onroute"), 
+        onSen("Onscene"), unAvail("Unavailable");
+        
+        private String status;   
+        Status(String status){
+            this.status = status;
+        }
+        
+        public String getStatus() {
+            return status;
+        }
+    }
+
+    private final String id;
+    private String callSign;
+    private String currLocation;
+    private String currEvent;
+    private String time;
+    private final Type type;
+    private Status status;
+    
+    /*Units() {
+        this.id = "id";
+        this.callSign = "callSign";
+        this.currLocation = "currLocation";
+        this.currEvent = "currEvent";
+        this.time = "time";
+        this.type = setType("car1");
+        this.status = setStatus("avail");
+    }*/
+    
+    Units(String uniqId, String callSign, String currLocation, String currEvent, String time, String type, String stat) {
+        this.id = uniqId;
+        this.callSign = callSign;
+        this.currLocation = currLocation;
+        this.currEvent = currEvent;
+        this.time = time; //not final TBD 'Type'
+        this.type = setType(type);
+        this.status = setStatus(stat);
+    }
+    
+     public void printInfo(){
+        System.out.println("ID: " + id);
+        System.out.println("Call Sign: " + callSign);
+        System.out.println("Location: " + currLocation);
+        System.out.println("Current Event: " + currEvent);
+        System.out.println("Time: " + time);
+        System.out.println("Type: " + type.getType());
+        System.out.println("Status: " + status.getStatus());
+    }   
+//setters
+    public String changeLocation(String unitID){ 
+        return "changeLocation("+"'"+ unitID +"'"+ ")"; 
+    }
+    
+    public String setLocation(String unitID) {
+        return "setLocation("+"'"+ unitID +"'"+ ")"; 
+    }
+    
+    public String createMarker(String unitID) { 
+        return "createMarker("+"'"+ unitID +"'"+ ")"; 
+    }
+
+    public void setTime(String time) {
+        this.time = time; //not final TBD Type
+    }
+         
+    public Status setStatus(String status){ //may change
+        switch(status){
+            case "avail": return this.status.avail; 
+            case "unAvail": return this.status.unAvail; 
+            case "onRot": return this.status.onRot; 
+            case "onSen": return this.status.onSen; 
+            default: break;
+        }
+        return null;
+    }   
+    
+    public Type setType(String type){ //may change  
+        switch(type){
+            case "car1": return this.type.car1;
+            case "car2": return this.type.car2;
+            default: break;
+        }
+        return null;
+    }   
+    
+//getters    
+    public String getId(){
+        return id;
+    }
+    
+    public String getCallSign() {
+        return callSign;
+    }
+
+    public String getCurrLocation() {
+        return currLocation;
+    }
+
+    public String getCurrEvent() {
+        return currEvent;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public String getType(){
+        return type.getType();
+    }
+    
+    public String getStatus(){
+        return status.getStatus();
+    }
+}
